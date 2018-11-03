1492812efee92c8eec8cf77abec95fb9573f9bb6
El Capitan <Elcapitan@Els-MacBook-3.local>
2018-11-02 19:21:40 -0700

    update 11022018

--- main/urls.py
+++ main/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^', include('apps.Home.urls')),
@@ -25,3 +28,6 @@ urlpatterns = [
     url(r'^reservations/', include('apps.Reservations.urls', 'reservations'), name='reservations'),
     url(r'^reviews/', include('apps.Reviews.urls', 'reviews'), name='reviews')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
